dade877bd66aaaf1dcd56f0c302a016b341c82c7
Jsukrit <sukritdn1@gmail.com>
2025-08-02 23:43:40 +0700

    update

---
+++ src/component/Footer.tsx
@@ -0,0 +1,10 @@
+import { type FooterProps } from "../libs/types";
+
+export function Footer(props: FooterProps) {
+  return (
+    <footer className="text-secondary text-center p-2 bg-light mt-auto">
+      Copyright © {props.year} {props.fullName} {props.studentId}
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/component/Taskinput.tsx
@@ -0,0 +1,20 @@
+export function Task() {
+  return (
+    <div className="m-2 p-2">
+      <form className="row">
+        <div className="col-10">
+          <h1>Sukrit jitaree</h1>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Insert a task here.."
+          ></input>
+        </div>
+        <div className="col-2">
+          <button className="btn btn-primary">Add</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+export default Task;
